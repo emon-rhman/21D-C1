485e3975b8fc2b8ca476d4d6a698249e25446f5e
Emon Rahman <246264230+emon-rhman@users.noreply.github.com>
2025-12-03 00:29:47 +0600

    8th Day of 21D-C

---
+++ palindrom_number.py
@@ -0,0 +1,16 @@
+def isPalindrome(x: int) -> bool:
+    if x < 0:
+        return False
+
+    original = x
+    rev = 0
+
+    while x > 0:
+        rev = rev * 10 + x % 10
+        x //= 10
+
+    return rev == original
+
+
+n = int(input("Enter a number: "))
+print(isPalindrome(n))
