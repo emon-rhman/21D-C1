49e251e84d375e2e2c608d789b789c06779a5bb6
Emon Rahman <246264230+emon-rhman@users.noreply.github.com>
2025-12-12 20:45:13 +0600

    15th

---
+++ fourSum.py
@@ -0,0 +1,40 @@
+def four_sum(nums, target):
+    nums.sort()
+    n = len(nums)
+    res = []
+
+    for i in range(n):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
+        for j in range(i + 1, n):
+            if j > i + 1 and nums[j] == nums[j - 1]:
+                continue
+
+            left = j + 1
+            right = n - 1
+
+            while left < right:
+                s = nums[i] + nums[j] + nums[left] + nums[right]
+
+                if s == target:
+                    res.append([nums[i], nums[j], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+                elif s < target:
+                    left += 1
+                else:
+                    right -= 1
+
+    return res
+
+
+nums = list(map(int, input("Enter numbers: ").split()))
+target = int(input("Enter target: "))
+print(four_sum(nums, target))
